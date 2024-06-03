daca2e49d48e1508c618685dc160ee9b1c3bc310
Reizerva <160796063+Reizerva@users.noreply.github.com>
2024-06-03 00:53:29 +0000

    Entrada 2
    não consegui terminar (confundi i e j umas 10 vezes ;-;)

--- tabuleiro/tabuleiro_jogo.c
+++ tabuleiro/tabuleiro_jogo.c
@@ -2,8 +2,9 @@
 #include <stdlib.h>
 
 int main() {
-  char tabuleiro[8][8];
-  int i, j;
+  char tabuleiro[8][8]; // se tão na mesma linha os is são iguais, se tão na mesma coluna os jotas são iguais
+  int i/* mesma linha*/, j/*mesma coluna*/; // posição da peça no tabuleiro
+  int h, k; // para onde a peça quer ir
   for (i = 0; i < 8; i++) {
     for (j = 0; j < 8; j++) {
       if ((i + j) % 2 == 0) {
@@ -19,10 +20,10 @@ int main() {
       }
     }
   }
-
-  int deu_certo = system("clear");
-  int reprint = 1;
+  int deu_certo;
+  int reprint = 1; //reprintar o tabuleiro
   while (reprint == 1) {
+    deu_certo = system("clear");
     for (i = 0; i < 8; i++) {
       for (j = 0; j < 8; j++) {
         printf(" %c", tabuleiro[i][j]);
@@ -30,33 +31,80 @@ int main() {
       printf(" %d\n", (i + 1));
     }
     printf(" 1 2 3 4 5 6 7 8\n");
+    reprint = 0;
   }
-  int vez = 0; // 0 é Jogador 0, 1 é Jogador X
+  int vez = 0; // 0 é Jogador 0, 1 é Jogador X, no 2 alguem venceu
   int peca_tem_moves = 0;
   while (vez == 1) {
     printf("X, escolha a peça que deseja mover (linha coluna): ");
     deu_certo = scanf("%d %d", &i, &j);
-    while (getchar() != '\n')
-      ;
-    if (tabuleiro[i - 1][j - 1] != 'X') {
-      printf("Nenhuma peça X encontrada.\n");
+    i = i-1;
+    j = j-1;
+    while (getchar() != '\n');
+    if (tabuleiro[i][j] != 'X') {
+      printf("Nenhuma peça X encontrada. Precione ENTER para escolher novamente\n");
+      getchar();
+      i = 0;
+      j = 0;
+      h = 0;
+      k = 0;
     } else if (j == 0 || j == 7) {
       if (j == 0) {
-        if (tabuleiro[i - 1][j + 1] == ' ') {
+        if (tabuleiro[i - 2][j] == ' ') {
           peca_tem_moves = 1;
-        } else if (tabuleiro[i - 1][j + 1] == '0') {
-          if (tabuleiro[i - 2][j + 2] == ' ' || tabuleiro[i-2][j-2] == ' '){
-            peca_tem_moves = 1;
-          }
+        }
+      } else {
+        if (tabuleiro[i - 2][j - 2] == ' ') {
+          peca_tem_moves = 1;
         }
       }
-      if (peca_tem_moves == 1) {
-        printf("X, escolha a posição para onde deseja mover a peça (linha coluna): ");
-        deu_certo = scanf("%d %d", &i, &j);
-        while (getchar() != '\n')
-          ;
+    } else {
+      if (tabuleiro[i - 2][j - 2] == ' ' || tabuleiro[i - 2][j] == ' ') {
+        peca_tem_moves = 1;
+      } else if (tabuleiro[i - 2][j - 2] == 'O' || tabuleiro[i - 2][j] == 'O') {
+        if (tabuleiro[i - 3][j - 3] == ' ' || tabuleiro[i - 3][j + 1] == ' ') {
+          peca_tem_moves = 1;
+        }
       }
     }
+    if (peca_tem_moves == 1) {
+      printf("X, escolha a posição para onde deseja mover a peça (linha coluna): ");
+      deu_certo = scanf("%i %i", &h, &k);
+      h = h - 1;
+      k = k - 1;
+      while (getchar() != '\n');
+      if (tabuleiro[h - 1][k - 1] == ' ') {
+        if (h - 1 == i - 2) {
+          if (k - 1 == j || k - 1 == j - 2) {
+            tabuleiro[i - 1][j - 1] = ' ';
+            tabuleiro[h - 1][k - 1] = 'X';
+            vez = 0;
+          }
+        }
+      } else if (tabuleiro[h-1][k-1] == 'O') {
+        if (tabuleiro[i-3][j-3] == ' ') {
+          tabuleiro[h-1][k-1] = ' ';
+          tabuleiro[i-1][j-1] = ' ';
+          tabuleiro[i-3][j-3] = 'X';
+        } else if (tabuleiro[i-3][j] == ' ') {
+          tabuleiro[h-1][k-1] = ' ';
+          tabuleiro[i-1][j-1] = ' ';
+          tabuleiro[i-3][j] = 'X';
+        }
+      }
+    }
+    if (vez == 1) {
+      printf("Nenhum movimento possível para a peça escolhida. Precione ENTER para escolher novamente \n");
+      getchar();
+      i = 0;
+      j = 0;
+      h = 0;
+      k = 0;
+    }
+  }
+  while (vez == 0) {}
+  while (vez == 2){
+    
   }
   return 0;
-}
+  }
