928f6fa1c9bc782b429f96268126ada5d918ce60
trsaints <thiago.dev2k3@gmail.com>
2024-06-05 21:58:56 -0300

    update: renomear propriedades do jogo

--- tabuleiro/tabuleiro_jogo.c
+++ tabuleiro/tabuleiro_jogo.c
@@ -3,11 +3,11 @@
 
 struct jogo
 {
-  int reprint,
-      vez, // 0 é Jogador 0, 1 é Jogador X, 2 é novo_jogo.reprint
-      proxima_vez,
-      ganhou,
-      peca_tem_moves,
+  int _REPRINT,
+      estado, // 0 é Jogador 0, 1 é Jogador X, 2 é novo_jogo.reprint
+      _ACAO_ATUAL,
+      partida_acabou,
+      pode_mover,
       pecas_X,
       pecas_O;
 };
@@ -15,156 +15,173 @@ struct jogo
 int main()
 {
   int entrada_correta = 0;
-  char tabuleiro[8][8]; // se tão na mesma linha os is são iguais, se tão na mesma coluna os jotas são iguais
-  int i, j;             // posição da peça no tabuleiro
-  int h, k;             // para onde a peça quer ir
+  char tabuleiro[8][8];
+  // se tão na mesma linha os is são iguais, se tão na mesma coluna os jotas são iguais
+  int linha_origem, coluna_origem;   // posição da peça no tabuleiro
+  int linha_destino, coluna_destino; // para onde a peça quer ir
 
-  for (i = 0; i < 8; i++)
-    for (j = 0; j < 8; j++)
+  for (linha_origem = 0; linha_origem < 8; linha_origem++)
+    for (coluna_origem = 0; coluna_origem < 8; coluna_origem++)
     {
-      if ((i + j) % 2 == 0)
-        tabuleiro[i][j] = ' ';
+      if ((linha_origem + coluna_origem) % 2 == 0)
+        tabuleiro[linha_origem][coluna_origem] = ' ';
       else
       {
-        if (i < 3)
-          tabuleiro[i][j] = 'O';
-        else if (i > 4)
-          tabuleiro[i][j] = 'X';
+        if (linha_origem < 3)
+          tabuleiro[linha_origem][coluna_origem] = 'O';
+        else if (linha_origem > 4)
+          tabuleiro[linha_origem][coluna_origem] = 'X';
         else
-          tabuleiro[i][j] = ' ';
+          tabuleiro[linha_origem][coluna_origem] = ' ';
       }
     }
 
   struct jogo novo_jogo = {1, 2, 1, 0, 0, 12, 12};
 
-  while (novo_jogo.ganhou == 0)
+  while (novo_jogo.partida_acabou == 0)
   {
-    while (novo_jogo.vez == 2)
+    while (novo_jogo.estado == 2)
     {
-      if (novo_jogo.reprint == 1)
+      if (novo_jogo._REPRINT == 1)
       {
         entrada_correta = system("clear");
 
-        for (i = 0; i < 8; i++)
+        for (linha_origem = 0; linha_origem < 8; linha_origem++)
         {
-          for (j = 0; j < 8; j++)
-            printf(" %c", tabuleiro[i][j]);
+          for (coluna_origem = 0; coluna_origem < 8; coluna_origem++)
+            printf(" %c", tabuleiro[linha_origem][coluna_origem]);
 
-          printf(" %d\n", (i + 1));
+          printf(" %d\n", (linha_origem + 1));
         }
 
         printf(" 1 2 3 4 5 6 7 8\n");
-        novo_jogo.reprint = 0;
+        novo_jogo._REPRINT = 0;
       }
 
-      novo_jogo.vez = novo_jogo.proxima_vez;
+      novo_jogo.estado = novo_jogo._ACAO_ATUAL;
     }
 
-    while (novo_jogo.vez == 1)
+    while (novo_jogo.estado == 1)
     {
-      if (novo_jogo.pecas_X == 0 || novo_jogo.pecas_O == 0)
+      int pecas_esgotaram = novo_jogo.pecas_X == 0 || novo_jogo.pecas_O == 0;
+
+      if (pecas_esgotaram)
       {
-        novo_jogo.vez = 2;
-        novo_jogo.reprint = 1;
-        novo_jogo.proxima_vez = 3;
+        novo_jogo.estado = 2;
+        novo_jogo._REPRINT = 1;
+        novo_jogo._ACAO_ATUAL = 3;
       }
 
       printf("X, escolha a peça que deseja mover (linha coluna): ");
 
-      entrada_correta = scanf("%d %d", &i, &j),
-      i = i - 1,
-      j = j - 1;
+      entrada_correta = scanf("%d %d", &linha_origem, &coluna_origem),
+      linha_origem = linha_origem - 1,
+      coluna_origem = coluna_origem - 1;
 
       while (getchar() != '\n')
         ;
 
-      if (tabuleiro[i][j] != 'X')
+      int nao_tem_x = tabuleiro[linha_origem][coluna_origem] != 'X';
+
+      if (nao_tem_x)
       {
         printf("Nenhuma peça X encontrada. Precione ENTER para escolher novamente\n");
         getchar();
 
-        i = 0,
-        j = 0,
-        h = 0,
-        k = 0;
+        linha_origem = 0,
+        coluna_origem = 0,
+        linha_destino = 0,
+        coluna_destino = 0;
       }
-      else if (tabuleiro[i][j] == 'X')
+      else
       {
-        if (tabuleiro[i - 1][j + 1] == ' ' || tabuleiro[i - 1][j - 1] == ' ')
-          novo_jogo.peca_tem_moves = 1;
-        if (tabuleiro[i - 1][j + 1] == 'O' && tabuleiro[i - 2][j + 2] == ' ')
-          novo_jogo.peca_tem_moves = 1;
-        if (tabuleiro[i - 1][j - 1] == 'O' && tabuleiro[i - 2][j - 2] == ' ')
-          novo_jogo.peca_tem_moves = 1;
+        int tem_diagonal_livre = tabuleiro[linha_origem - 1][coluna_origem + 1] == ' ' || tabuleiro[linha_origem - 1][coluna_origem - 1] == ' ';
+        int peca_colidiu_a_esquerda = tabuleiro[linha_origem - 1][coluna_origem + 1] == 'O' && tabuleiro[linha_origem - 2][coluna_origem + 2] == ' ';
+        int peca_colidiu_a_direita = tabuleiro[linha_origem - 1][coluna_origem - 1] == 'O' && tabuleiro[linha_origem - 2][coluna_origem - 2] == ' ';
+
+        if (tem_diagonal_livre)
+          novo_jogo.pode_mover = 1;
+
+        if (peca_colidiu_a_esquerda)
+          novo_jogo.pode_mover = 1;
+
+        if (peca_colidiu_a_direita)
+          novo_jogo.pode_mover = 1;
       }
-      if (novo_jogo.peca_tem_moves == 1)
+
+      if (novo_jogo.pode_mover == 1)
       {
         printf("X, escolha a posição para onde deseja mover a peça (linha "
                "coluna): ");
 
-        entrada_correta = scanf("%i %i", &h, &k),
-        h = h - 1,
-        k = k - 1;
+        entrada_correta = scanf("%i %i", &linha_destino, &coluna_destino),
+        linha_destino = linha_destino - 1,
+        coluna_destino = coluna_destino - 1;
 
         while (getchar() != '\n')
           ;
 
-        if (h == i - 1)
+        int movimento_valido = linha_destino == linha_origem - 1;
+
+        if (movimento_valido)
         {
-          if (k == j + 1 || k == j - 1)
+          int pode_ir_para_diagonal = coluna_destino == coluna_origem + 1 || coluna_destino == coluna_origem - 1;
+          int peca_colide_em_diagonal = tabuleiro[linha_destino][coluna_destino] == 'O';
+
+          if (pode_ir_para_diagonal)
           {
-            if (tabuleiro[h][k] == ' ')
+            if (tabuleiro[linha_destino][coluna_destino] == ' ')
             {
-              tabuleiro[h][k] = 'X';
-              tabuleiro[i][j] = ' ';
-              novo_jogo.proxima_vez = 0;
-              novo_jogo.vez = 2;
+              tabuleiro[linha_destino][coluna_destino] = 'X';
+              tabuleiro[linha_origem][coluna_origem] = ' ';
+              novo_jogo._ACAO_ATUAL = 0;
+              novo_jogo.estado = 2;
             }
-            else if (tabuleiro[h][k] == 'O')
+            else if (peca_colide_em_diagonal) // tabuleiro[h][k] != 'X'
             {
-              if (k < j)
+              if (coluna_destino < coluna_origem)
               {
-                if (tabuleiro[i - 2][j - 2] == ' ')
+                if (tabuleiro[linha_origem - 2][coluna_origem - 2] == ' ')
                 {
-                  tabuleiro[h][k] = ' ';
-                  tabuleiro[i][j] = ' ';
-                  tabuleiro[i - 2][j - 2] = 'X';
+                  tabuleiro[linha_destino][coluna_destino] = ' ';
+                  tabuleiro[linha_origem][coluna_origem] = ' ';
+                  tabuleiro[linha_origem - 2][coluna_origem - 2] = 'X';
                   novo_jogo.pecas_O = novo_jogo.pecas_O - 1;
-                  novo_jogo.proxima_vez = 0;
-                  novo_jogo.vez = 2;
+                  novo_jogo._ACAO_ATUAL = 0;
+                  novo_jogo.estado = 2;
                 }
               }
-              else if (k > j)
+              else if (coluna_destino > coluna_origem)
               { // isso é um fallback, deve da 1 sempre
-                if (tabuleiro[i - 2][j + 2] == ' ')
+                if (tabuleiro[linha_origem - 2][coluna_origem + 2] == ' ')
                 {
-                  tabuleiro[h][k] = ' ';
-                  tabuleiro[i][j] = ' ';
-                  tabuleiro[i - 2][j + 2] = 'X';
+                  tabuleiro[linha_destino][coluna_destino] = ' ';
+                  tabuleiro[linha_origem][coluna_origem] = ' ';
+                  tabuleiro[linha_origem - 2][coluna_origem + 2] = 'X';
                   novo_jogo.pecas_O = novo_jogo.pecas_O - 1;
-                  novo_jogo.proxima_vez = 0;
-                  novo_jogo.vez = 2;
+                  novo_jogo._ACAO_ATUAL = 0;
+                  novo_jogo.estado = 2;
                 }
               }
             }
           }
         }
-        if (novo_jogo.vez == 1)
+        if (novo_jogo.estado == 1)
         {
           printf("Nenhum movimento possível para a peça escolhida. Precione "
                  "ENTER para escolher novamente\n");
           getchar();
-          i = 0;
-          j = 0;
-          h = 0;
-          k = 0;
-          novo_jogo.reprint = 1;
-          novo_jogo.vez = 2;
-          novo_jogo.proxima_vez = 1;
+          linha_origem = 0;
+          coluna_origem = 0;
+          linha_destino = 0;
+          coluna_destino = 0;
+          novo_jogo._REPRINT = 1;
+          novo_jogo.estado = 2;
+          novo_jogo._ACAO_ATUAL = 1;
         }
       }
     }
-    while (novo_jogo.vez == 0)
+    while (novo_jogo.estado == 0)
     {
       printf("Teste deu certo");
 
@@ -172,7 +189,7 @@ int main()
       while (getchar() != '\n')
         ;
 
-      novo_jogo.vez = 3;
+      novo_jogo.estado = 3;
     }
   }
 
